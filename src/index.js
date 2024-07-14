7ddcbbf787e40a82786cd96b6d56147b032d002f
Tinkjimmy <tinkjimmy938@gmail.com>
2024-07-14 19:32:29 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
@@ -9,9 +10,9 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   
   <React.StrictMode>
-    <Router basename="/">
-      <App />
-    </Router>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+     <App />
+   </BrowserRouter>
   </React.StrictMode>
 
 );
