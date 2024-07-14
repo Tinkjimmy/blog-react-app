48f966ebe6f02faf77c06b0c58e39b9e08029a89
Tinkjimmy <tinkjimmy938@gmail.com>
2024-07-14 19:17:59 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   
   <React.StrictMode>
-    <Router basename="/blog-react-app">
+    <Router basename="/">
       <App />
     </Router>
   </React.StrictMode>
